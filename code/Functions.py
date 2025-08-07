acf7e35a075da6351df49468ca69038a5df27991
Florian Gruber <grub.florianp@gmail.com>
2025-08-08 00:24:41 +0200

    15.1

--- code/Functions.py
+++ code/Functions.py
@@ -10,63 +10,54 @@ import pyclesperanto as cle
 from PIL import Image
 
 
-size_min = 150
-size_max = 2000
 
-file_select = glob.glob(r"C:\Users\grufl\Desktop\211007 Sicherung USB\uni\Prakt bioinfo\Bioinfo\data\raw\selected-tiles\out_opt_flow_registered_X10_Y10_*.tif")
-
-binary_list = []
-#nuclei_count = []
-data = {}
-# dictionary für jedes file?
-
-for file in file_select:
-    data[file] = []
-    #data["file"].append(file)
-    image = ski.io.imread(file)
-    # Apply Gaussian filter to denoise the image
-    image_denoised = gaussian(image, sigma=2, preserve_range=True)
-    image_binary = image_denoised > ski.filters.threshold_li(image_denoised)
-    split_objects = split_touching_objects(image_binary)
-    # using connected component labeling
-    labeled_objects = ski.measure.label(split_objects, connectivity=1)  
-    # exclude labels out of size range
-    filtered_labels = cle.exclude_labels_outside_size_range(labeled_objects, minimum_size=size_min, maximum_size=size_max)  
-    # for counting nuclei
-    num_features = filtered_labels.max()  
-    data[file].append(num_features)
-    # Convert filtered labels to monochrome (binary) image
-    monochrome = (filtered_labels > 0).astype(np.uint8)
+def nuclei_count(file_select, size_min, size_max):
+    data = {}
+    binary_list = []
     
-    binary_list.append(monochrome)
+    for file in file_select:
+        data[file] = []
+        image = ski.io.imread(file)
+        # Apply Gaussian filter to denoise the image
+        image_denoised = gaussian(image, sigma=2, preserve_range=True)
+        image_binary = image_denoised > ski.filters.threshold_li(image_denoised)
+        split_objects = split_touching_objects(image_binary)
+        # using connected component labeling
+        labeled_objects = ski.measure.label(split_objects, connectivity=1)  
+        # exclude labels out of size range
+        filtered_labels = cle.exclude_labels_outside_size_range(labeled_objects, minimum_size=size_min, maximum_size=size_max)  
+        # for counting nuclei
+        num_features = filtered_labels.max()  
+        data[file].append(num_features)
+        # Convert filtered labels to monochrome (binary) image
+        monochrome = (filtered_labels > 0).astype(np.uint8)
+        binary_list.append(monochrome)
 
 #saving images as PDF
     
 # Set your output directory and file name
-output_dir = r"C:\Users\grufl\Desktop\211007 Sicherung USB\uni\Prakt bioinfo\Bioinfo\data\processed/images"
-os.makedirs(output_dir, exist_ok=True)
+    output_dir = r"C:\Users\grufl\Desktop\211007 Sicherung USB\uni\Prakt bioinfo\Bioinfo\data\processed/images"
+    os.makedirs(output_dir, exist_ok=True)
 
-for idx, img in enumerate(binary_list):
-    # Erzeuge einen Dateinamen, z.B. monochrome_0.pdf, monochrome_1.pdf, ...
-    pdf_path = os.path.join(output_dir, f"monochrome_{idx+1}.pdf")
+    for idx, img in enumerate(binary_list):
+        # Erzeuge einen Dateinamen, z.B. monochrome_0.pdf, monochrome_1.pdf, ...
+        pdf_path = os.path.join(output_dir, f"monochrome_{idx+1}.pdf")
     # Falls img ein pyclesperanto-Array ist, zuerst zu numpy holen:
-    if hasattr(img, "get"):
-        img = img.get()
-    pil_img = Image.fromarray((img * 255).astype('uint8')).convert('L')
-    pil_img.save(pdf_path)
-    print(f"Gespeichert: {pdf_path}")
+        if hasattr(img, "get"):
+            img = img.get()
+        pil_img = Image.fromarray((img * 255).astype('uint8')).convert('L')
+        pil_img.save(pdf_path)
+        print(f"Gespeichert: {pdf_path}")
 
 # making a csv file
-df = pd.DataFrame(data)
-csv_file_path = r"C:\Users\grufl\Desktop\211007 Sicherung USB\uni\Prakt bioinfo\Bioinfo\data\processed\csv\analysis_results.csv"
-df.to_csv(csv_file_path, index=False)
-print(f'CSV file &quot;{csv_file_path}&quot; has been created successfully.')
+    df = pd.DataFrame(data)
+    csv_file_path = r"C:\Users\grufl\Desktop\211007 Sicherung USB\uni\Prakt bioinfo\Bioinfo\data\processed\csv\analysis_results.csv"
+    df.to_csv(csv_file_path, index=False)
+    print(f'CSV file &quot;{csv_file_path}&quot; has been created successfully.')
 
-fig, axs = plt.subplots(4, 6, figsize=(25, 15))
-for i, ax in enumerate(axs.flatten()):
-    ax.imshow(binary_list[i])
-plt.show()
-
-print(data)
-    
+    fig, axs = plt.subplots(4, 6, figsize=(25, 15))
+    for i, ax in enumerate(axs.flatten()):
+        ax.imshow(binary_list[i])
+    plt.show()
+    print(data)
     
